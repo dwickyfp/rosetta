3b23970892c2a2edfc761830004440e0237c3f99
Dwicky Feiransyah Putra <dwicky.f.putra@devbz.local>
2026-01-29 14:15:38 +0700

    feat: improve dlq

--- web/src/features/pipelines/components/postgres-table-config.tsx
+++ web/src/features/pipelines/components/postgres-table-config.tsx
@@ -176,7 +176,7 @@ export function PostgresTableConfig({
                   )}
                 >
                   <Code2 className="h-3 w-3 mr-1.5" />
-                  Custom
+                  Custom SQL
                 </Button>
               </div>
             )}
